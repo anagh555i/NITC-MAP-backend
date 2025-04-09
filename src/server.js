bcb97095ffd699d97f14230ffc2a88721424abb1
anagh555i <anagh555i@gmail.com>
2025-04-09 08:13:31 +0530

    added fix2

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cors({
-    origin: 'https://nitc-map.vercel.app', // https://nitc-map.vercel.app http://localhost:5173  
+    origin: 'http://localhost:5173', // https://nitc-map.vercel.app http://localhost:5173  
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     credentials: true
 }));
